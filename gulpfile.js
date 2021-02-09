cd0aeb2d7b776815069fd88bf358c259b2592aa8
Jonatha Horsman <jonathan.e.horsman@gmail.com>
2021-02-08 17:51:18 -0800

    added deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var browsersync = require('browser-sync').create()
 
 var imagemin = require("gulp-imagemin")
 
+var ghpages = require('gh-pages')
 
 sass.compiler = require('node-sass')
 
@@ -50,6 +51,11 @@ gulp.task('watch', function(){
     gulp.watch("src/css/app.scss", ["sass"])
     gulp.watch('src/fonts/*', ["fonts"])
     gulp.watch('src/img/*', ["images"])
+})
+
+
+gulp.task('deploy', function(){
+    ghpages.publish("dist")
 })
 
 gulp.task('default', ["html", "sass", "fonts", "images", "watch"])
